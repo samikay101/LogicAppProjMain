4a057e3f2a3d34681107c6e0f675948c405cb872
Sami Karakra <samikarakra@microsoft.com>
2025-03-20 11:00:44 -0700

    restore nuget command

--- Tests/samiApp1/obj/Debug/net6.0/samiApp1.AssemblyInfo.cs
+++ Tests/samiApp1/obj/Debug/net6.0/samiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("samiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747fb29b6c213073876cbd382aefb3dd09e61854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865c5392ed1f570e430576addf64d8952489d792")]
 [assembly: System.Reflection.AssemblyProductAttribute("samiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("samiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
